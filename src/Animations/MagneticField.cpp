39c158169f63e24c7acbba98a0f6f4cd44bb43d0
Maël CRD <mael.cainjo@gmail.com>
2026-07-07 13:13:23 +0200

    feat: Improve MagneticField animation

--- src/Animations/MagneticField.cpp
+++ src/Animations/MagneticField.cpp
@@ -5,7 +5,6 @@
 #include <cmath>
 #include <cstdint>
 #include <cstdlib>
-#include <functional>
 #include <initializer_list>
 #include <unistd.h>
 
@@ -18,15 +17,44 @@ void MagneticField::animate(double time) {
   offscreen_canvas->Clear();
 
   // Create a new magnetic element with a certain probability
-  if (rand() % 60 < 1) { // % chance of spawning a new magnetic element
+  if ((rand() % 60 < 1 && magnetic_elements.size() < 5) ||
+      magnetic_elements.size() <=
+          2) { // % chance of spawning a new magnetic element
     MagneticElement element;
-    element.x = rand() % offscreen_canvas->width();
-    element.y = rand() % offscreen_canvas->height();
-    element.life = (rand() % 10 + 20) / 10.0;
+
+    // Try to spawn the element at a random position, but ensure it is not too
+    // close to existing elements
+    bool valid_position = false;
+    uint16_t max_attempts = 200;
+    while (!valid_position && max_attempts > 0) {
+      valid_position = true;
+      element.x = rand() % offscreen_canvas->width();
+      element.y = rand() % offscreen_canvas->height();
+
+      for (const auto &existing_element : magnetic_elements) {
+        float dx = existing_element.x - element.x;
+        float dy = existing_element.y - element.y;
+        float distance_squared = dx * dx + dy * dy;
+        if (distance_squared < 5500.0f) { // Minimum distance squared
+          valid_position = false;
+          break;
+        }
+      }
+      max_attempts--;
+    }
+    // std::cout << "Max attempts left: " << max_attempts << std::endl;
+
+    element.life = (rand() % 10 + 20) / 3.0;
     element.age = 0;
-    element.color[0] = rand() % 256;
-    element.color[1] = rand() % 256;
-    element.color[2] = rand() % 256;
+
+    // Random hue between 0-20 and 265-360 degrees
+    float hue =
+        (265 + rand() % (95 + 20)) % 360; // Random hue between 200-360 degrees
+    utils::hsvToRgb(hue, 0.8f, 1.0f, r, g, b);
+    element.color[0] = r;
+    element.color[1] = g;
+    element.color[2] = b;
+
     magnetic_elements.push_back(element);
   }
 
@@ -49,63 +77,76 @@ void MagneticField::animate(double time) {
     for (int y = 1; y < offscreen_canvas->height(); y += 3) {
       float force_x = 0;
       float force_y = 0;
+      float mixed_r = 0, mixed_g = 0, mixed_b = 0;
       for (const auto &element : magnetic_elements) {
         float dx = element.x - x;
         float dy = element.y - y;
-        float distance_squared = dx * dx + dy * dy;
+        float distance_squared = pow(dx, 2) + pow(dy, 2);
         if (distance_squared < 1.0f)
           distance_squared = 1.0f; // Avoid division by zero
-        float force_magnitude =
-            1000000.0f / distance_squared * element.strength;
-        force_x += force_magnitude * (dx / sqrt(distance_squared));
-        force_y += force_magnitude * (dy / sqrt(distance_squared));
+        float force_magnitude = element.strength / distance_squared;
+        force_x += force_magnitude * dx;
+        force_y += force_magnitude * dy;
+
+        mixed_r += element.color[0] * force_magnitude;
+        mixed_g += element.color[1] * force_magnitude;
+        mixed_b += element.color[2] * force_magnitude;
       }
-      // Normalize the force
-      float force_magnitude =
-          sqrt((force_x * force_x + force_y * force_y)) * 0.1f;
-      force_magnitude = std::min(force_magnitude, 255.0f);
 
       // Draw a line representing the force vector (in a 3x3 area)
       float angle = atan2(force_y, force_x);
       int angle_index =
           static_cast<int>(round((angle + M_PI) / (M_PI * 2.0) * 8.0));
+
+      rgb_matrix::Color color(255, 255, 255);
+      rgb_matrix::Color color_dark(255, 0, 0);
+      rgb_matrix::Color color_light(190, 0, 0);
       int dx = 0, dy = 0;
       switch (angle_index) {
       case 0: // Right (pt. 1)
         dx = 1;
         dy = 0;
+        color = color_dark;
         break;
       case 1: // Down-Right
         dx = 1;
         dy = 1;
+        color = color_light;
         break;
       case 2: // Down
         dx = 0;
         dy = 1;
+        color = color_dark;
         break;
       case 3: // Down-Left
         dx = 1;
         dy = -1;
+        color = color_light;
         break;
       case 4: // Left
         dx = -1;
         dy = 0;
+        color = color_dark;
         break;
       case 5: // Up-Left
         dx = -1;
         dy = -1;
+        color = color_light;
         break;
       case 6: // Up
         dx = 0;
         dy = -1;
+        color = color_dark;
         break;
       case 7: // Up-Right
         dx = 1;
         dy = -1;
+        color = color_light;
         break;
       case 8: // Right (pt. 2)
         dx = 1;
         dy = 0;
+        color = color_dark;
         break;
       default:
         dx = 0;
@@ -113,13 +154,19 @@ void MagneticField::animate(double time) {
         break;
       }
 
+      float max_mixed_value = std::max({mixed_r, mixed_g, mixed_b});
+      mixed_r = (mixed_r / max_mixed_value) * 255.0f;
+      mixed_g = (mixed_g / max_mixed_value) * 255.0f;
+      mixed_b = (mixed_b / max_mixed_value) * 255.0f;
+      color = rgb_matrix::Color(static_cast<uint8_t>(mixed_r),
+                                static_cast<uint8_t>(mixed_g),
+                                static_cast<uint8_t>(mixed_b));
+
       int x1 = x - dx;
       int y1 = y - dy;
       int x2 = x + dx;
       int y2 = y + dy;
-      uint8_t r, g, b = static_cast<uint8_t>(force_magnitude);
-      rgb_matrix::DrawLine(offscreen_canvas, x1, y1, x2, y2,
-                           rgb_matrix::Color(255, 255, 255));
+      rgb_matrix::DrawLine(offscreen_canvas, x1, y1, x2, y2, color);
 
       // r = static_cast<uint8_t>(force_magnitude);
       // g = static_cast<uint8_t>(255 - force_magnitude);
